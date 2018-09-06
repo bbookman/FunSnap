c91189a86af82e9ddca556a5db4065040d5c7a78
Unknown <bruce.bookman@gmail.com>
2018-09-05 17:58:53 -0700

    get user data

--- FunSnap/ViewController.swift
+++ FunSnap/ViewController.swift
@@ -25,10 +25,41 @@ class ViewController: UIViewController {
             
             if success {
                 print("Login Successful")
+                self.getUserDetails()
             }
         }
         
     }
+    
+    
+    func getUserDetails() {
+        
+        let graphQLQuery = "{me{displayName, bitmoji{avatar}, externalId}}"
+        
+        let variables = ["page": "bitmoji"]
+        
+        SCSDKLoginClient.fetchUserData(withQuery: graphQLQuery, variables: variables, success: { (resources: [AnyHashable: Any]?) in
+            guard let resources = resources,
+                let data = resources["data"] as? [String: Any],
+                let me = data["me"] as?  [String: Any] else { return }
+            
+            let displayName = me["displayName"] as? String
+            
+            print((String(describing: displayName)))
+            
+            var bitmojiAvatarUrl: String?
+            if let bitmoji = me["bitmoji"] as? [String: Any] {
+                bitmojiAvatarUrl = bitmoji["avatar"] as? String
+                print((String(describing: bitmojiAvatarUrl)))
+            }
+        }, failure: { (error: Error?, isUserLoggedOut: Bool) in
+            if let error = error {
+                print(error.localizedDescription)
+            }
+        })
+    }
+    
+    
     
 }
 
